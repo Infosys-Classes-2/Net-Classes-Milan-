7708dddb9d4dafdc9d812485908bc17fe03d01c6
Milan_Dhakal <milandhakal186@gmail.com>
2022-07-28 09:36:31 +0545

    Comments added

--- CSharpFundamentals/Program.cs
+++ CSharpFundamentals/Program.cs
@@ -1,14 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
 
+//  Day 1 work 
 
 Console.Write("What is your FullName?");
 var fullName =Console.ReadLine();
 var len= fullName.IndexOf(" ");
 var firstName = fullName.Substring(0,len);
 
-
-
 Console.WriteLine($"Hello, {firstName}");
 
+
+// Day 2 work
 
