94db04677795597c7bbdd805e52026d6a06b3c61
Milan_Dhakal <milandhakal186@gmail.com>
2022-07-28 09:47:47 +0545

    Uppercase work done

--- CSharpFundamentals/Program.cs
+++ CSharpFundamentals/Program.cs
@@ -12,4 +12,9 @@ Console.WriteLine($"Hello, {firstName}");
 
 
 // Day 2 work
+// 
+Console.WriteLine("Enter the text to convert to UPPERCASE:");
+string text =Console.ReadLine();
+string converted = text.ToUpper();
+Console.WriteLine($"The Converted Text is: {converted}");
 
