3e205e71613820a1bf545488573d21a525f4f69c
Milan_Dhakal <milandhakal186@gmail.com>
2022-08-03 10:56:38 +0545

    more on methods

--- CSharpFundamentals/Lessons/0EntryPoint.cs
+++ CSharpFundamentals/Lessons/0EntryPoint.cs
@@ -1,38 +1,37 @@
 using System;
-class Test 
+class Test
 {
-    public static void Main ()
+    public static void Main()
     {
-       /*  Animal cow =new();
-        Animal goat=new();
-        Animal buffalo =new();
-        Animal deer = new ();
-        Animal fish =new Animal();
+        /*  Animal cow =new();
+         Animal goat=new();
+         Animal buffalo =new();
+         Animal deer = new ();
+         Animal fish =new Animal(); */
 
-        
-        /* Console.WriteLine("Enter the weight of the animal");
-        cow.weight = Console.Readline() ; */
-        /* cow.type = "cow";
-        cow.PrintDetails(); */ 
-        
-        
-     /*    BranchesAndLoops br = new();
-       // br.LearnBranching();
-        br.PatternOne(); */
 
-       /*  Pattern1 patt = new();
-        patt.Pattern2(); */
-         
-        /*  PatternWithFunction patternWithFunction =new();
+         /*  Console.WriteLine("Enter the weight of the animal");
+          cow.weight = Console.Readline() ; 
+          cow.type = "cow";
+          cow.PrintDetails();  */
+
+
+        /* BranchesAndLoops br = new();
+           br.LearnBranching();
+           br.PatternOne(); */
+
+        /*  Pattern1 patt = new();
+         patt.Pattern2(); */
+
+        /* PatternWithFunction patternWithFunction =new();
            patternWithFunction.PatternOneWithFunction(20);
+           patternWithFunction.PrintPatternTwoWithFunction(20); */
 
-          patternWithFunction.PrintPatternTwoWithFunction(20); */
+        MethodsLearning methodsLearning = new();
 
-          MethodsLearning methodsLearning =new();
-
-          short[] ages ={12,11,54,656,76,434,654,32,46,67,86,};
-          var (min , max ,average) = methodsLearning.GetMinMaxAndAverage(ages);
-          Console.WriteLine($"The minimum value is:{min}, The Maximum value:{max}, The average is {average}");
+        short[] ages = { 12, 11, 54, 656, 76, 434, 654, 32, 46, 67, 86, };
+        var (min, max) = methodsLearning.GetMinMaxAndAverageDeclarative(ages);
+        Console.WriteLine($"The minimum value is:{min}, The Maximum value:{max},");
 
     }
 }
