4f674f1024f81888389848f35ac7ba785610d71f
Milan_Dhakal <milandhakal186@gmail.com>
2022-08-09 10:27:53 +0545

    PolyMorphism

---
+++ CSharpFundamentals/Lessons/B2-Properties.cs
@@ -0,0 +1,38 @@
+using System;
+
+//Properties
+
+public class Planet
+{
+    private string name;
+    //Full Property
+    public string Name{
+      get
+      {
+        return name;
+      }
+      set
+      {
+        if (value.Length >5)
+        name=value;
+      }
+    }
+  // Auto Implemented Property 
+  //for readonly property Remove Set
+    public double Mass{get; set;}
+// ReadOnly Property
+// In line initialize garna use => '='
+    public double DistanceFromSun { get;  }
+   
+}
+
+public class Test3
+{
+    void Do()
+    {
+        Planet earth = new();
+        earth.Name ="Earth";
+        earth.Mass=354.34534; 
+
+    }
+}
