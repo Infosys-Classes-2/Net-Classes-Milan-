ce3a1ee4e7c99a204769120aa99e10107f596b73
Milan_Dhakal <milandhakal186@gmail.com>
2022-08-10 18:28:13 +0545

    Completed fibonacci series co

--- CSharpFundamentals/Assignments/Assignment 3/classes Assignment3/ProductOfDigits.cs
+++ CSharpFundamentals/Assignments/Assignment 3/classes Assignment3/ProductOfDigits.cs
@@ -1,22 +1,32 @@
 using System;
-class ProductOfDigit{
 
-   public string number; 
+class ProductOfDigit
+{
+    public string number;
 
     internal int GetProduct(string number)
-   {
-
-    int product=1;
-    for(int i =0; i< number.Length; i++)
+    {
+        int product = 1;
+        for (int i = 0; i < number.Length; i++)
         {
-            product = product* (number[i] - 48) ;
+            product = product * (number[i] - 48);
         }
         return product;
-
-
-   }
-
+    }
 }
 
-
-
+class Fibonacci
+{
+    internal int FindFibonacci(int num)
+    {
+        if (num <= 1)
+        {
+           return num;
+        }
+        else{
+        
+           return FindFibonacci (num - 1) + FindFibonacci(num - 2);
+        }
+    
+    }
+}
