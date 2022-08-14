72a519c3093547c75d17efbaf4e0b97aebdd363d
Milan_Dhakal <milandhakal186@gmail.com>
2022-08-14 10:53:52 +0545

    Completed Fibonacci with Iterative

--- CSharpFundamentals/Assignments/Assignment 3/classes Assignment3/ProductOfDigits.cs
+++ CSharpFundamentals/Assignments/Assignment 3/classes Assignment3/ProductOfDigits.cs
@@ -19,23 +19,30 @@ class Fibonacci
 {
     internal int FindFibonacci(int num)
     {
-        if (num <= 1)
+        if (num <= 2)
         {
-           return num;
+           return num -1 ;
         }
         else{
         
-           return FindFibonacci (num - 1) + FindFibonacci(num - 2);
+           return FindFibonacci (num - 1) 
+           + FindFibonacci(num - 2);
         }
     
     }
-}
 
-class DecimalToBinaryConversion
-{
-    int[] Convert(){
+     internal long GetNthFibIterative(int n)
+    {
+        long[] series = new long[n];
+        series[0] = 0;
+        series[1] = 1;
 
-        
+        for (int i = 2; i < n; i++)
+        {
+            series[i] = series[i - 1] + series[i - 2];
+        }
+
+        return series[n - 1];
     }
-    
 }
+
