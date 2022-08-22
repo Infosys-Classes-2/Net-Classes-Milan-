d7b802a0925570caddc03e1e8d9338c06a5d7ef1
Milan_Dhakal <milandhakal186@gmail.com>
2022-08-22 10:56:36 +0545

    Study web File Structure and layout

---
+++ HelloWeb/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using HelloWeb.Models;
+
+namespace HelloWeb.Controllers;
+public class HomeController : Controller
+{
+   
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    public string test()
+    {
+        return "Hello there";
+    }
+
+}
---
+++ HelloWeb/ReadMe.txt
@@ -0,0 +1,10 @@
+Environments
+
+
+
+- SDLC
+1- Development (Dev)
+2- QA or Testing or Tst
+3- INT or integration (Regression Testing)
+4- UAT <User Acceptance Testing>
+5- Porducton or Prod(LIVE Version)
