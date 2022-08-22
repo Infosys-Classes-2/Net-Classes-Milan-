fdcf436d59d3aece7542ba01b3ee536a70afce05
Milan_Dhakal <milandhakal186@gmail.com>
2022-08-22 22:49:18 +0545

    Completed 10 rows in tables

---
+++ FirstWebAssignment/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace FirstWebAssignment.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
