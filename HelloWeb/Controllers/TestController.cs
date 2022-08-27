0a3ef075d6376cd18a3d0f9ac119735468f08fc2
Milan_Dhakal <milandhakal186@gmail.com>
2022-08-27 21:49:19 +0545

    first web assignmet complete

---
+++ HelloWeb/Controllers/TestController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelloWeb.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Multiply(int num)
+        {
+            return View(num);
+        }
+        public IActionResult AssignmentTwo() 
+        {
+            List<int> numbers = new List<int>();
+            return View(); 
+        }
+    }
+}
